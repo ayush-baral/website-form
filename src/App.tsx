d2b9411f6d6d3c82a86cc86b63873f7b1ef7208a
dark-king-rayleigh <ayushbaral17@gmail.com>
2022-06-28 15:54:50 +0545

    feat(Home):create page and define route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Routes>
+      {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
